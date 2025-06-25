98234d67a28b8dd2d680c7ebcc662cb75c29a562
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-06-25 20:21:17 +0100

    feat: generated poll module for api

--- monitoring-app/apps/api/src/app.module.ts
+++ monitoring-app/apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MetricsModule } from './metrics/metrics.module';
+import { PollsModule } from './polls/polls.module';
 
 @Module({
-  imports: [MetricsModule],
+  imports: [MetricsModule, PollsModule],
   controllers: [AppController],
   providers: [AppService],
 })
