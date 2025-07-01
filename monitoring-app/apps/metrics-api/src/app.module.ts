dbf9103e3b79f7acf1b8a5910b268ae2e8520dec
Jack Francis Dalton <jackfrancisdalton@gmail.com>
2025-07-01 12:26:53 +0100

    remove: removed monitoring api app as decided it was prefrable to have a metrics module in api instead

--- monitoring-app/apps/metrics-api/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MetricsModule } from './metrics/metrics.module';
-
-@Module({
-  imports: [MetricsModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
--- monitoring-app/apps/metrics-api/src/metrics/metrics.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MetricsController } from './metrics.controller';
-import { MetricsService } from './metrics.service';
-
-@Module({
-  providers: [MetricsService],
-  controllers: [MetricsController]
-})
-export class MetricsModule {}
